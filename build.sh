469e92198f9d46d95bbd652d1587c4bb87becd77
VanShane <vanhoeckshane@gmail.com>
2024-09-25 12:05:36 +0200

    update file sctucture + create crowdin workflow

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+declare -a languageMap=(
+  "cs-CS"
+  "de-DE"
+  "el-GR"
+  "en-GB"
+  "es-ES"
+  "fr-FR"
+  "it-IT"
+  "ja-JP"
+  "pl-PL"
+  "pt-BR"
+  "pt-PT"
+  "ru-RU"
+  "sv-SE"
+  "th-TH"
+  "tr-TR"
+  "zh-TW"
+)
+
+# create distribution directory
+mkdir ./distribution
+
+# create asset for each language
+for lang in "${languageMap[@]}"; do
+  pushd ./translations/"$lang" || exit
+  zip -r "$OLDPWD"/distribution/"$lang".zip .
+  popd || exit
+done
